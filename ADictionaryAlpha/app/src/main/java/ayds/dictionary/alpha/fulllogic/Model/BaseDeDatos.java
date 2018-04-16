8059bd98754ab83bce479e6b848917bbf906e876
igsub <igsub@hotmail.com>
2018-04-16 09:35:38 -0300

    Eliminacion de cascara de base de datos

--- ADictionaryAlpha/app/src/main/java/ayds/dictionary/alpha/fulllogic/Model/BaseDeDatos.java
+++ ADictionaryAlpha/app/src/main/java/ayds/dictionary/alpha/fulllogic/Model/BaseDeDatos.java
@@ -1,28 +1,48 @@
 package ayds.dictionary.alpha.fulllogic.Model;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.content.Context;
+
+import ayds.dictionary.alpha.fulllogic.room.ConceptDataBase;
 
 
 public class BaseDeDatos implements DataConPersistencia {
 
     private Context context;
+    private ConceptDataBase db;
 
-    public BaseDeDatos(Context context){
-        this.context=context;
+
+    public BaseDeDatos(Context context) {
+        this.context = context;
     }
 
     @Override
     public String obtenerDefinicion(String term) {
-        return DataBase.getMeaning(term);
+
+        Term concept = db.termDao().findByName(term);
+
+        if (concept != null) {
+            return concept.getDefinicion();
+        }
+        return null;
+
     }
 
     @Override
     public void conectar() {
-        DataBase.createNewDatabase(context);
+
+        db = Room.databaseBuilder(context,
+                ConceptDataBase.class, "dictionary.db").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+
     }
 
-    public void guardarTermino(Term termino){
-        DataBase.saveTerm(termino.getNombre(),termino.getDefinicion());
+    public void guardarTermino(Term termino) {
+
+        Term concept = new Term();
+        concept.setNombre(termino.getNombre());
+        concept.setDefinicion(termino.getDefinicion());
+        concept.setSource(1);
+        db.termDao().insert(concept);
     }
 }
