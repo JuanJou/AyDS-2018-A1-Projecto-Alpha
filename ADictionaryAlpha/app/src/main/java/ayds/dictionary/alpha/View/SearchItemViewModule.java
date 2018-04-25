e96d1ac588753d7aa64214edbde2ab4240ecb5b7
Luciano Fuentes <lucho.fuentez@gmail.com>
2018-04-25 08:50:45 -0300

    Cambios 3

--- ADictionaryAlpha/app/src/main/java/ayds/dictionary/alpha/View/SearchItemViewModule.java
+++ ADictionaryAlpha/app/src/main/java/ayds/dictionary/alpha/View/SearchItemViewModule.java
@@ -1,4 +1,31 @@
 package ayds.dictionary.alpha.View;
 
+import android.content.Context;
+
 public class SearchItemViewModule {
+
+    private static SearchItemViewModule instance;
+    private Context context;
+
+    private SearchItemViewModule() {
+
+    }
+
+    public static SearchItemViewModule getInstance() {
+        if (instance == null) {
+            instance = new SearchItemViewModule();
+        }
+        return instance;
+    }
+
+    void setContext(Context context) {
+
+        this.context = context;
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+
 }
